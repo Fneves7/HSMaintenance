eeb3947e4ca22eb45318c4e890267737a9ff8fe0
Francisco Neves <fnneves93@gmail.com>
2023-12-04 21:02:27 +0000

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#script for starting a minecraft server
+
+#changedir
+cd /home/server/1201
+
+#start server deatached
+java -Xms6656M -Xmx6656M --add-modules=jdk.incubator.vector -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -jar server.jar --nogui
